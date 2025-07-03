a3c6a1ce60a43ff59294de4a17dd5c35b1a4fc65
Joey Wei <joeywei1318@icloud.com>
2025-07-03 11:40:10 -0500

    Create calendar.js

---
+++ js/calendar.js
@@ -0,0 +1,5 @@
+// Simple calendar placeholder
+document.addEventListener("DOMContentLoaded", function () {
+  const calendar = document.getElementById("calendar");
+  calendar.innerHTML = "<p>[Calendar will be implemented here]</p>";
+});
